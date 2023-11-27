6a302985dbfb867d0cb963192d8bcff0f18def38
simoni-git <145527459+simoni-git@users.noreply.github.com>
2023-11-27 21:45:59 +0900

    MVC --> MVVM
    MVC --> MVVM

---
+++ QuotesGenerator/ViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  ViewModel.swift
+//  QuotesGenerator
+//
+//  Created by MAC on 11/27/23.
+//
+
+import Foundation
+
+class ViewModel {
+    
+    var quetos: [Quote] = [
+        Quote(contents: "죽음을 두려워하는 나머지 삶을 시작조차 하지 못하는 사람이 많다.", name: "벤다이크"),
+        Quote(contents: "나는 나자신을 빼놓고는 모두 안다.", name: "비용"),
+        Quote(contents: "편견이란 실효성이 없는 의견이다", name: "암브로스 빌"),
+        Quote(contents: "분노는 바보들의 가슴속에서만 살아간다", name: "아인슈타인"),
+        Quote(contents: "몇번이라도 좋다..이 끔찍한 생이여..다시!!", name: "니체"),
+        Quote(contents: "스위프트 존나어렵다..", name: "시몬") ,
+        Quote(contents: "오늘부터 열심히산댄다", name: "동주")
+    ]
+    
+}
